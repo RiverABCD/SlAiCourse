fe9ee35bf513cebf02aab9ac01f66c8200f48b12
River <ljiuang@gmail.com>
2019-09-08 17:52:40 +0800

    version1.1_add slatewidget

--- Intermediate/ProjectFiles/SSlAiRayInfoWidget.cpp
+++ Intermediate/ProjectFiles/SSlAiRayInfoWidget.cpp
@@ -2,17 +2,40 @@
 
 #include "SSlAiRayInfoWidget.h"
 #include "SlateOptMacros.h"
+#include "SlAiGameWidgetStyle.h"
+#include "SlAiStyle.h"
 
+#include "SBorder.h"
+#include "SBox.h"
+#include "STextBlock.h"
 
 BEGIN_SLATE_FUNCTION_BUILD_OPTIMIZATION
 void SSlAiRayInfoWidget::Construct(const FArguments& InArgs)
 {
-	
+	//获取GameStyle
+	GameStyle = &SlAiStyle::Get().GetWidgetStyle<FSlAiGameStyle>("BPSlAiGameStyle");
 
-	/*
 	ChildSlot
 		[
-		];*/
-	
+			SNew(SBox).WidthOverride(400.f).HeightOverride(100.f)
+			[
+				SNew(SBorder).BorderImage(&GameStyle->RayInfoBrush).HAlign(HAlign_Center).VAlign(VAlign_Center)
+				[
+					SAssignNew(RayInfoTextBlock, STextBlock).Font(GameStyle->Font_Outline_50)
+					.ColorAndOpacity(GameStyle->FontColor_Black)
+				]
+			]
+		];
+
 }
 END_SLATE_FUNCTION_BUILD_OPTIMIZATION
+
+void SSlAiRayInfoWidget::Tick(const FGeometry & AllottedGeometry, const double InCurrentTime, const float InDeltaTime)
+{
+	//第一帧的时候进行初始化
+	if (!IsInitRayInfoEvent)
+	{
+		RegisterRayInfoEvent.ExecuteIfBound(RayInfoTextBlock);
+		IsInitRayInfoEvent = true;
+	}
+}
--- Intermediate/ProjectFiles/SSlAiRayInfoWidget.h
+++ Intermediate/ProjectFiles/SSlAiRayInfoWidget.h
@@ -4,9 +4,14 @@
 
 #include "CoreMinimal.h"
 #include "Widgets/SCompoundWidget.h"
+#include "DeclarativeSyntaxSupport.h"
 
+
+class STextBlock;
+
+DECLARE_DELEGATE_OneParam(FRegisterRayInfoEvent, TSharedPtr<STextBlock>)
 /**
- * 
+ *
  */
 class SLAICOURSE_API SSlAiRayInfoWidget : public SCompoundWidget
 {
@@ -15,7 +20,18 @@ public:
 	{}
 	SLATE_END_ARGS()
 
-	/** Constructs this widget with InArgs */
-	void Construct(const FArguments& InArgs);
+		/** Constructs this widget with InArgs */
+		void Construct(const FArguments& InArgs);
 
+	virtual void Tick(const FGeometry& AllottedGeometry, const double InCurrentTime, const float InDeltaTime) override;
+public:
+	FRegisterRayInfoEvent RegisterRayInfoEvent;
+
+private:
+	//保存显示射线信息的文本
+	TSharedPtr<STextBlock> RayInfoTextBlock;
+	//获取GameStyle
+	const struct FSlAiGameStyle* GameStyle;
+	//是否已经初始化事件
+	bool IsInitRayInfoEvent;
 };
