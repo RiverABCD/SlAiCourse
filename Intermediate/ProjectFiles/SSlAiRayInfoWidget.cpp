dd8df429b032d53381e01d8422b3220c200030c2
River <ljiuang@gmail.com>
2019-09-08 16:47:55 +0800

    add slatewidget real successfully?

---
+++ Intermediate/ProjectFiles/SSlAiRayInfoWidget.cpp
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "SSlAiRayInfoWidget.h"
+#include "SlateOptMacros.h"
+
+
+BEGIN_SLATE_FUNCTION_BUILD_OPTIMIZATION
+void SSlAiRayInfoWidget::Construct(const FArguments& InArgs)
+{
+	
+
+	/*
+	ChildSlot
+		[
+		];*/
+	
+}
+END_SLATE_FUNCTION_BUILD_OPTIMIZATION
